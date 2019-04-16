fe85803ce1583ea2e0b5da14d7900123379c5ceb
YU1ut <imikushana@gmail.com>
2019-04-16 15:57:30 +0900

    add

--- code/utils/cluster/data.py
+++ code/utils/cluster/data.py
@@ -266,13 +266,13 @@ def _create_dataloaders(config, dataset_class, tf1, tf2,
         root=config.dataset_root,
         transform=tf1,
         split=train_partition,
-        target_transform=target_transform)
+        target_transform=target_transform, download=True)
     else:
       train_imgs_curr = dataset_class(
         root=config.dataset_root,
         transform=tf1,
         train=train_partition,
-        target_transform=target_transform)
+        target_transform=target_transform, download=True)
 
     if hasattr(config, "mix_train"):
       if config.mix_train and (train_partition == "train+unlabeled"):
